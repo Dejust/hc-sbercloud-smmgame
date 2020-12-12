a88de9d7a963e4d6adbf11344417722e50eb1f2e
Vadim Sultanov <vadim.sultanov@gcorelabs.com>
2020-12-12 17:32:52 +0500

    SMMGAME2 renaming

--- smmgame2/urls.py
+++ smmgame2/urls.py
@@ -1,4 +1,4 @@
-"""ssmgame2 URL Configuration
+"""smmgame2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
