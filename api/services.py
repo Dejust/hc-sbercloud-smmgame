1982981431fd864e851849d3c75f0cb6938814d5
Vadim Sultanov <vadim.sultanov@gcorelabs.com>
2020-12-13 16:37:53 +0500

    fix rates

--- api/services.py
+++ api/services.py
@@ -5,8 +5,8 @@ def aggregate_user_rate(queryset):
     return queryset.aggregate(score=functions.Coalesce(Sum('score'), 0))
 
 
-def aggregate_all_user_rates(queryset):
-    return queryset.values('user_id').annotate(total_scores=Sum('score')).order_by('total_scores')
+def aggregate_all_user_rates(queryset, limit=3):
+    return queryset.values('user_id').annotate(total_scores=Sum('score')).order_by('-total_scores')[:limit]
 
 
 def get_achievements(queryset):
